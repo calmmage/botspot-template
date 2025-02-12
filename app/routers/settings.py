e6dd532da693fd3e0cc2428aafde9035f8c052f9
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-02-12 08:35:37 +0300

    v0.1.0 - Update template for new botspot version

--- app/routers/settings.py
+++ app/routers/settings.py
@@ -2,9 +2,9 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from botspot.components.bot_commands_menu import add_command
-from botspot.utils import reply_safe, answer_safe
-from botspot.components.ask_user_handler import ask_user, ask_user_choice
+from botspot.components.features.ask_user_handler import ask_user, ask_user_choice
+from botspot.components.settings.bot_commands_menu import add_command
+from botspot.utils import reply_safe
 
 router = Router()
 
